e07090fd6c1df7c0b6e3afda679c7c3b78c9e04b
linsy_king <linsy_king@sjtu.edu.cn>
2022-04-11 11:02:01 +0800

    Fix

--- lifa/check.py
+++ lifa/check.py
@@ -6,19 +6,20 @@ from winsound import Beep
 
 def sendreq(rdat):
     while(1):
-        res = requests.post('https://sports.sjtu.edu.cn/manage/fieldDetail/queryFieldSituation',json=rdat,cookies={"JSESSIONID":"6d6435db-3b80-459a-af09-a4adacb1522a"})
+        res = requests.get(f'https://dailyreport.sjtu.edu.cn/haircut/frontend/bus/schedule/list?{rdat}',cookies={"JSESSIONID":"***", "dailyreport.sjtu": "***"})
         kop = res.content.decode(encoding='utf-8',errors='ignore')
-        kks = json.loads(kop)['data'][0]['priceList']
+        kks = json.loads(kop)['entities']
+        print(rdat[10])
         for one in kks:
-            if(int(one['count'])!=0):
+            if(int(one['leftSeat'])!=0):
                 Beep(1000,1000)
-                if(rdat['fieldType']=='0a14f823-3ac7-48e8-9313-4d7545e6c430'):
+                if(rdat[10]=='W'):
                     print('2餐')
-                if(rdat['fieldType']=='12add9f5-a3b3-4caf-a9ab-803867bac856'):
+                if(rdat[10]=='H'):
                     print('3餐')
-                if(rdat['fieldType']=='5b7887ed-2525-4950-b490-8315919c6fb2'):
+                if(rdat[10]=='O'):
                     print('4餐')
-                print(rdat['date'])
+                print(rdat['stateDate'],rdat['startTime'])
                 break
             # print(one['count'])
         sleep(3)
@@ -29,13 +30,10 @@ def seq(ddts):
 
 sp=[]
 
-for i in range(9,10):
-    if(i==9):
-        i='09'
-    date=f'2022-04-{i}'
-    sp.append({"fieldType":"0a14f823-3ac7-48e8-9313-4d7545e6c430","date":date})
-    sp.append({"fieldType":"12add9f5-a3b3-4caf-a9ab-803867bac856","date":date})
-    sp.append({"fieldType":"5b7887ed-2525-4950-b490-8315919c6fb2","date":date})
+for i in range(11,12):
+    sp.append(f'lineType=TWO&date=2022-04-{i}')
+    sp.append(f'lineType=THIRD&date=2022-04-{i}')
+    sp.append(f'lineType=FOURTH&date=2022-04-{i}')
 
 alth=[]
 
